d1694ad792c1f518551a0419684fb416fcc5959c
gabino mendez <gabino_mendez@hotmail.com.ar>
2022-09-01 17:21:10 -0300

    socket-db

---
+++ DB/mysqlDB/connectionDB.js
@@ -0,0 +1,12 @@
+const options={
+    client:'mysql',
+    connection:{
+        host: '127.0.0.1', 
+        user: 'root',
+        password:'lebron23JAMES',
+        database:'ecommerce'
+    }
+}
+module.exports={
+    options
+}
